71e915e8973251458fe0779766dc1425621141f6
JBamford1 <30533757+JBamford1@users.noreply.github.com>
2018-10-25 13:21:32 -0700

    Initial commit

---
+++ MainModule/SampleModule.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace PrismxMaterialDesign.MainModule
+{
+    class SampleModule : IModule
+    {
+        IRegionManager _manager;
+
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            _manager = containerProvider.Resolve<IRegionManager>();
+            var regions = _manager.Regions;
+            regions["MainRegion"].Add(containerProvider.Resolve<MainView>());
+            // This will throw an exception because "RightDrawerRegion" doesn't exist
+            //regions["RightDrawerRegion"].Add(containerProvider.Resolve<RightDrawerView>());
+            // However this works
+            _manager.RegisterViewWithRegion("RightDrawerRegion", typeof(RightDrawerView));
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry) {}
+    }
+}
